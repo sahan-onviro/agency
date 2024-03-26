4d267d6eb18174a03471d1b4a891f03681546007
sahan-onviro <sahan.onviro@gmail.com>
2024-03-26 15:40:13 +0545

    agency

--- src/Pages/Homepage/section/RegisterForm.jsx
+++ src/Pages/Homepage/section/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -7,6 +7,16 @@ import { formData } from '../../../Global/Data/Navbar';
 const RegisterForm = () => {
     const typeData = formData?.find((item) => item.name === 'type').option;
     const countryData = formData?.find((item) => item.name === 'country').option;
+    const [imageName, setImageName] = useState("No Files Selected");
+    const handleFileChange = (event) => {
+        const inputImage = event.target.files[0];
+        if (inputImage) {
+            setImageName(inputImage.name);
+        } else {
+            setImageName("No Files Selected");
+        }
+
+    }
     return (
         <>
             <section className="h-registerform bg-waves section-break">
@@ -111,9 +121,24 @@ const RegisterForm = () => {
                                                     <Field placeholder="Website URL" name="website" className='form-control' onChange={formik.handleChange}></Field>
                                                     <ErrorMessage component='div' name="website" className='form-error' />
                                                 </div>
-                                                <div className="form-group">
+                                                <div className="form-group photo-group">
                                                     <label className='form-label'>Upload Photo</label>
-                                                    <Field name="photo" type="file" className='form-control' onChange={formik.handleChange}></Field>
+                                                    <Field name="photo" type="file" className='form-control'
+                                                        onChange={(event) => {
+                                                            formik.handleChange(event);
+                                                            handleFileChange(event);
+                                                        }}></Field>
+                                                    <div class="btn-group">
+                                                        <span class="photo-btn">Upload Photo
+                                                            <svg width="14" height="17" viewBox="0 0 14 17" fill="none"
+                                                                xmlns="http://www.w3.org/2000/svg">
+                                                                <path
+                                                                    d="M5 12.5924H9C9.55 12.5924 10 12.1424 10 11.5924V6.59244H11.59C12.48 6.59244 12.93 5.51244 12.3 4.88244L7.71 0.292444C7.61749 0.19974 7.5076 0.126193 7.38662 0.0760114C7.26565 0.02583 7.13597 0 7.005 0C6.87403 0 6.74435 0.02583 6.62338 0.0760114C6.5024 0.126193 6.39251 0.19974 6.3 0.292444L1.71 4.88244C1.08 5.51244 1.52 6.59244 2.41 6.59244H4V11.5924C4 12.1424 4.45 12.5924 5 12.5924ZM1 14.5924H13C13.55 14.5924 14 15.0424 14 15.5924C14 16.1424 13.55 16.5924 13 16.5924H1C0.45 16.5924 0 16.1424 0 15.5924C0 15.0424 0.45 14.5924 1 14.5924Z"
+                                                                    fill="#F5F7FD" />
+                                                            </svg>
+                                                        </span>
+                                                        <p id="imageName">{imageName}</p>
+                                                    </div>
                                                     <ErrorMessage component='div' name="photo" className='form-error' />
                                                 </div>
 
